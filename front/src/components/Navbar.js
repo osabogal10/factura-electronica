5f32e988905567381e4d3cb4ed605ed0d959240f
Andrés Felipe López <af.lopezf@uniandes.edu.co>
2018-09-15 21:28:52 -0500

    Se cambia el estado con la navbar y se iniciaron los componentes Login y Signup

--- front/src/components/Navbar.js
+++ front/src/components/Navbar.js
@@ -1,41 +1,38 @@
 import React, {Component} from "react";
 import 'bootstrap/dist/css/bootstrap.css';
-import "../style/Navbar.css"
+import "../style/Navbar.css";
 import registerServiceWorker from "./../registerServiceWorker";
 
 /**
- * This class contains all needed to display the nav bar on top.
- */
+* This class contains all needed to display the nav bar on top.
+*/
 export default class Navbar extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            onChange: props.onChange
-        };
-    }
-
-    render() {
-        return (
-            <div className="fixed-top">
-                <nav id="navbar_home" className="nav navbar-light">
-                    <a id="nombre_nav" className="col-md-7 navbar-brand hvr-underline-from-center" href={""}
-                       onClick={this.state.onChange.bind(this, "inicio")}>
-                        <img src={require("./../img/newsletter.png")} width="30" height="30"
-                             className="d-inline-block align-top" alt=""/> Factura Electronica
-                    </a>
-                    <a className="nav-item nav-link " href={""}
-                       onClick={this.state.onChange.bind(this, "inicio")}>Inicio</a>
-                    <a className="nav-item nav-link " href={""} onClick={this.state.onChange.bind(this, "login")}>Log
-                        In</a>
-                    <a className="nav-item nav-link " href={""} onClick={this.state.onChange.bind(this, "signin")}>Sign
-                        Up</a>
-                </nav>
-
-            </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      onChange: props.onChange
+    };
+  }
+  
+  render() {
+    return (
+      <div className="fixed-top">
+        <nav id="navbar_home" className="nav navbar-light">
+          <a id="nombre_nav" className="col-md-7 navbar-brand hvr-underline-from-center" 
+            onClick={this.state.onChange.bind(this, 'Home')}>
+            <img src={require("./../img/newsletter.png")} width="30" height="30"
+              className="d-inline-block align-top" alt=""/> Factura Electronica
+          </a>
+          <a className="nav-item nav-link "  onClick={this.state.onChange.bind(this, 'Home')}>Inicio</a>
+          <a className="nav-item nav-link "  onClick={this.state.onChange.bind(this, 'Login')}>Log In</a>
+          <a className="nav-item nav-link "  onClick={this.state.onChange.bind(this, 'Signup')}>Sign Up</a>
+        </nav>
+      
+      </div>
+    );
+  }
 }
-
-
+  
+  
 registerServiceWorker();
